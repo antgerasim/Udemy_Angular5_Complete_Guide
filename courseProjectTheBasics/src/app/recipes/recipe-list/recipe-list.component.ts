9778d6ac2e8e235b85e55965f540d48580cc521f
The don <donantoine@mail.ru>
2018-04-28 18:13:12 +0300

    commit

--- courseProjectTheBasics/src/app/recipes/recipe-list/recipe-list.component.ts
+++ courseProjectTheBasics/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,6 @@ import { Component, OnInit } from '@angular/core';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
-<<<<<<< HEAD
     new Recipe('A Test Recipe', 'This is simply a test', 'https://www.maxpixel.net/static/photo/1x/Meat-Power-Recipe-Food-Dishes-Pork-1459693.jpg'),
     new Recipe('Second Test Recipe', 'This is second test', 'https://c.pxhere.com/photos/50/03/beirut_power_supply_snack-1375814.jpg!d'),
     new Recipe('Third Test Recipe', 'This is a third test', 'https://www.nps.gov/subjects/camping/images/recipe_1.jpg?maxwidth=1200&maxheight=1200&autorotate=false')
@@ -17,11 +16,6 @@ export class RecipeListComponent implements OnInit {
 
 
 
-=======
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://ic.pics.livejournal.com/stalic/2762948/529356/529356_original.jpg')
-  ];
-
->>>>>>> afb5f53e44fc51ad7d3bb50521fe1f12b78206f5
   constructor() { }
 
   ngOnInit() {
